61aa4e5bd8a3f85cadfc5b2e97bed2b40b58d4e3
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-10 16:33:54 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! Update and rename README.rst to README.md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="odoo-migrate",
-    version="0.0.5",
+    version="0.0.6",
     author="GRAP, Groupement Régional Alimentaire de Proximité",
     author_email="informatique@grap.coop",
     description="Small tools too migrate Odoo modules",
