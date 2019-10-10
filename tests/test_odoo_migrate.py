19b8b2eb16a8a64e9c2527b4f232492c040ae908
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-10 12:49:51 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! [FIX] flake8 + [WIP] Tox

---
+++ tests/test_odoo_migrate.py
@@ -0,0 +1,18 @@
+# import os
+# import tempfile
+import unittest
+# import kaptan
+# from textwrap import dedent
+
+# from git_aggregator import config
+# from git_aggregator.exception import ConfigException
+# from git_aggregator._compat import PY2
+
+
+print("fromage")
+
+
+class TestConfig(unittest.TestCase):
+
+    def _parse_config(self, config_str):
+        pass
