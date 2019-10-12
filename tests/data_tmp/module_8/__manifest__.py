ef139642e9b42a540302ddeb7adfa0948e5f8e1a
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-10-12 09:38:33 +0200

    fixup! fixup! [REF] module_8: Black python code

--- tests/data_tmp/module_8/__manifest__.py
+++
@@ -1,7 +0,0 @@
-# Copyright <YEAR(S)> <AUTHOR(S)>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-{
-    "name": "Module name",
-    "version": "12.0.1.0.0",
-    "installable": True,
-}
