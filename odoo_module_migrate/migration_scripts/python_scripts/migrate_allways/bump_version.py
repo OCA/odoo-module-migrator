723dc8cac727ace5be18b94189c5874689894e9e
Benoit Aimont <benoit.aimont@acsone.eu>
2021-07-16 14:22:50 +0200

    Add translations bump

--- odoo_module_migrate/migration_scripts/python_scripts/migrate_allways/bump_version.py
+++ odoo_module_migrate/migration_scripts/python_scripts/migrate_allways/bump_version.py
@@ -1,8 +1,29 @@
+import os
+
+
 def bump_revision(**kwargs):
     tools = kwargs['tools']
     manifest_path = kwargs['manifest_path']
     migration_steps = kwargs['migration_steps']
     target_version_name = migration_steps[-1]["target_version_name"]
+    init_version_name = migration_steps[0]["init_version_name"]
+
+    module_path = kwargs['module_path']
+    migration_path_folder = module_path / 'i18n'
+    file_paths = migration_path_folder.rglob("*")
+    line_to_change = '"Project-Id-Version: Odoo Server {}'
+    old_term = line_to_change.format(
+        init_version_name
+    )
+    new_term = line_to_change.format(
+        target_version_name
+    )
+    for file_path in file_paths:
+        if os.path.isfile(file_path):
+            tools._replace_in_file(
+                file_path,
+                {old_term: new_term}
+            )
 
     new_version = "%s.1.0.0" % target_version_name
 
