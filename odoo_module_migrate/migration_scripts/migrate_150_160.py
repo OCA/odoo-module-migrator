d2451d0b6c045aed93040a82167b231199306b15
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-10-10 11:35:34 +0200

    [IMP] Initialize scripts for 14.0-15.0 and 15.0-16.0

---
+++ odoo_module_migrate/migration_scripts/migrate_150_160.py
@@ -0,0 +1,7 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo_module_migrate.base_migration_script import BaseMigrationScript
+
+
+class MigrationScript(BaseMigrationScript):
+    pass
