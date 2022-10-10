de63e8249ef78879c9b94b1419db45b506ca150a
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-10-10 11:42:35 +0200

    [IMP] Rename get_xml_id to get_external_id (15 to 16)
    Ref: https://github.com/odoo/odoo/commit/8f7fad760196b2ad071e1e070a11003d9f02049f

--- odoo_module_migrate/migration_scripts/migrate_150_160.py
+++ odoo_module_migrate/migration_scripts/migrate_150_160.py
@@ -2,6 +2,12 @@
 
 from odoo_module_migrate.base_migration_script import BaseMigrationScript
 
+_TEXT_REPLACES = {
+    ".py": {
+        r"\.get_xml_id\(": ".get_external_id(",
+    },
+}
+
 
 class MigrationScript(BaseMigrationScript):
-    pass
+    _TEXT_REPLACES = _TEXT_REPLACES
