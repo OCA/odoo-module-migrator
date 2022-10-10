ae84b50dfc9b4183d83437c032757e450efbb596
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-10-10 14:12:13 +0200

    [IMP] Add unit tests for 15 to 16

---
+++ tests/data_template/module_150/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Module name",
+    "version": "15.0.1.0.0",
+    "installable": False,
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+}
