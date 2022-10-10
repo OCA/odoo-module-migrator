7edf2045301ac8db68aa82a005f461cf11c71115
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-10-10 11:46:17 +0200

    [IMP] Replace use of fields_get_keys. Use _fields instead (15 to 16)
    https://github.com/odoo/odoo/commit/8f7fad760196b2ad071e1e070a11003d9f02049f

--- odoo_module_migrate/migration_scripts/migrate_150_160.py
+++ odoo_module_migrate/migration_scripts/migrate_150_160.py
@@ -5,6 +5,7 @@ from odoo_module_migrate.base_migration_script import BaseMigrationScript
 _TEXT_REPLACES = {
     ".py": {
         r"\.get_xml_id\(": ".get_external_id(",
+        r"\.fields_get_keys\(\)": "._fields",
     },
 }
 
