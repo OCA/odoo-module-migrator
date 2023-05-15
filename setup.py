87de61f67aa38e85cde5211722623a4d0dbf66c9
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-05-15 10:24:11 +0200

    [REF] Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="odoo-module-migrator",
-    version="0.3.2",
+    version="0.4.0",
     author="GRAP, Groupement Régional Alimentaire de Proximité",
     author_email="informatique@grap.coop",
     description="Small tools to migrate Odoo modules from a version"
